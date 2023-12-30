a531948bbf9069d7a45d6f67e4ee6bc3db00d6e9
malavikavenugopal <malavikavenu914@gmail.com>
2023-12-30 21:41:38 +0530

    1st commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import ContextShare from './contexts/ContextShare';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextShare>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextShare>
+
   </React.StrictMode>
 );
 
